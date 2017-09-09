5d60a42e7c28daa377f9a5efd86152324845e88c
trautsned <sgbowden@asu.edu>
2017-09-09 14:56:28 -0700

    for pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-from distutils.core import setup
-
-setup(
-    name='photovoltaic',
-    version='0.1.0',
+from setuptools import setup
+setup(name='photovoltaic',
+    version='0.1.1',
     packages=['photovoltaic'],
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Physics',
+    ],
     url='https://github.com/trautsned/photovoltaic',
     license='GPLv3',
     author='Stuart Bowden',
     author_email='sgbowden@asu.edu',
-    description='Set of commonly used function in photovoltaics',
-	classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering :: Physics',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 3',
-
-	],
-    keywords='semiconductor solar pv',
-)
+    description='Set of commonly used functions in photovoltaics',
+    keywords='solar photovoltaic semiconductor',
+    install_requires=[
+          'numpy',
+    ])
